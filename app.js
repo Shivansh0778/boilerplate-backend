76670c3d1da8786772a6e2b4b9dedfb04e87d1a6
Shivansh Sharma <shivanshsharma0778@gmail.com>
2026-09-10 14:46:02 +0530

    added cors

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,17 +22,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "VIS Boilerplate Backend is running successfully"
-,    })
-})
+  res.json({
+    message: "VIS Boilerplate Backend is running successfully",
+  });
+});
 
 const startServer = async () => {
-    await connectDB();
+  await connectDB();
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    })
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 };
 
 startServer();
